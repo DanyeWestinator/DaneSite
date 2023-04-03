f0cfaaec21de1e4aacd4750ba37fc654eaee4d03
Dane Carstens <44176712+DanyeWestinator@users.noreply.github.com>
2023-04-02 17:09:17 -0700

    work on portfolio

--- Blogosphere/urls.py
+++ Blogosphere/urls.py
@@ -6,6 +6,7 @@ from . import views
 #global vars
 urlpatterns = [
     path("", views.Homepage, name="index"),
+    path("Index", views.Homepage, name="index"),
     path("Blogosphere", views.BlogosphereHome, name="index"),
     path(r"<int:id>/", views.entry, name = "entry"),
     path(r"Portfolio/<str:type>/", views.Portfolio, name="index")
